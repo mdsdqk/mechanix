4e0d2b7bb4553c87354758b8e662ca6e30375082
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-10-18 13:44:08 +0530

    added code to src

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
