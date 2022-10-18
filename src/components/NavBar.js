f5535362e18a659e4b1f3102f71dbe1b0aa088d2
Mohammed Sadiq K <md.sdq.k@gmail.com>
2022-10-18 13:44:25 +0530

    Add Navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../assets/mechanix-logo.png";
+
+export default function NavBar() {
+	return (
+		<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
+			<div className="container flex flex-wrap justify-between items-center mx-auto">
+				<Link href="/">
+					<div className="flex items-center mx-2">
+						<Image src={logo} width={36} height={36} className="mr-3" alt="Mechanix Logo" />
+						<span className="self-center text-xl font-semibold whitespace-nowrap mx-2">Mechanix</span>
+					</div>
+				</Link>
+				<div className="md:block md:w-auto" id="navbar-default">
+					<ul className="flex p-4 rounded-lg md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+						<li>
+							<Link href="#" className="block text-gray-700 rounded hover:text-blue-700 md:p-0">My Appointments</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+}
